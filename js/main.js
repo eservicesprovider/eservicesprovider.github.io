6412b55787f5ab6e085f8d2d941352f4ef0dc387
michael gordon <mgordonabc@gmail.com>
2018-03-04 10:18:12 -0500

    test buttons and add js/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,53 @@
-	  //check if already registered
+      //platform
+	  function onSignIn(googleUser) {
+		  var profile = googleUser.getBasicProfile();
+		  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+		  console.log('Name: ' + profile.getName());
+		  console.log('Image URL: ' + profile.getImageUrl());
+		  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-	  //check for phone number, get phone number if none
-	  //create cookie and storage
+		  //show logout button
+		  $("#google-auth-button").text("<a href='#' onclick='signOut();''>Sign out</a> &nbsp  &nbsp <a class='read' href='app/admin'> Admin </a> <button type='button' class='btn btn-outline-primary'>Primary</button>");	 
+      }
+
+   	  function signOut() {
+	    var auth2 = gapi.auth2.getAuthInstance();
+	    auth2.signOut().then(function () {
+	      console.log('User signed out.');
+	  });
+
+
+      //account manager
+	  function registerUser(){}
+
+	  function createXMRWallet(){}
+
+	  function createCryptoExchangeAccount(){}
+
+	  function getFriends(){}
+
+	  function getMissingData(){}
+
+	  function getMessages(){}
+
+	  function sendMessage(){}
+
+	  function writeToLocalDB(){}
+
+	  function readFromLocalDB(){}
+
+	  function writeBlockChainValue(){}
+
+	  function readBlockChainValue(){}
+
+	  //messages
+
+
+	  //friends
+
+
+	  //exchange
+
+	  //wallets
+
+	  //market
